f867cdd9feb4d25a1757487e3abaed65b26b499d
Adrien <adrien.bouteiller01@gmail.com>
2023-06-20 09:39:41 +0200

    up

--- src/script.js
+++ src/script.js
@@ -12,42 +12,76 @@ scene.add(sun);
 
 // Planets
 const planetsData = [
-  { name: "Mercury", radius: 0.05, distance: 4, speed: 0.001 },
+  {
+    name: "Mercury",
+    radius: 0.05,
+    distance: 4,
+    speed: 0.001,
+    info: "Information about Mercury",
+  },
   {
     name: "Venus",
     radius: 0.08,
     distance: 6,
     speed: 0.0008,
-    inclination: Math.PI / 8,
+    info: "Information about Venus",
   },
-  { name: "Earth", radius: 0.1, distance: 8, speed: 0.0006 },
+  {
+    name: "Earth",
+    radius: 0.1,
+    distance: 8,
+    speed: 0.0006,
+    info: "Information about Earth",
+  },
   {
     name: "Mars",
     radius: 0.07,
     distance: 10,
     speed: 0.0005,
-    inclination: Math.PI / 12,
+    info: "Information about Mars",
   },
-  { name: "Jupiter", radius: 0.18, distance: 14, speed: 0.0003 },
+  {
+    name: "Jupiter",
+    radius: 0.18,
+    distance: 14,
+    speed: 0.0003,
+    info: "Information about Jupiter",
+  },
   {
     name: "Saturn",
     radius: 0.15,
     distance: 18,
     speed: 0.0002,
-    inclination: Math.PI / 16,
+    info: "Information about Saturn",
   },
-  { name: "Uranus", radius: 0.12, distance: 22, speed: 0.0001 },
-  { name: "Neptune", radius: 0.11, distance: 26, speed: 0.00008 },
-  { name: "Pluto", radius: 0.04, distance: 30, speed: 0.00006 },
+  {
+    name: "Uranus",
+    radius: 0.12,
+    distance: 22,
+    speed: 0.0001,
+    info: "Information about Uranus",
+  },
+  {
+    name: "Neptune",
+    radius: 0.11,
+    distance: 26,
+    speed: 0.00008,
+    info: "Information about Neptune",
+  },
+  {
+    name: "Pluto",
+    radius: 0.04,
+    distance: 30,
+    speed: 0.00006,
+    info: "Information about Pluto",
+  },
 ];
 
 const planetMeshes = [];
 const orbitLines = [];
 
 planetsData.forEach((planetData) => {
-  const { radius, distance, speed, inclination = 0 } = planetData;
-
-  const planetGeometry = new THREE.SphereGeometry(radius, 32, 16);
+  const planetGeometry = new THREE.SphereGeometry(planetData.radius, 32, 16);
   const planetMaterial = new THREE.MeshLambertMaterial();
   const planet = new THREE.Mesh(planetGeometry, planetMaterial);
   scene.add(planet);
@@ -56,31 +90,18 @@ planetsData.forEach((planetData) => {
   const orbitCurve = new THREE.EllipseCurve(
     0,
     0,
-    distance,
-    distance,
+    planetData.distance,
+    planetData.distance,
     0,
     2 * Math.PI,
     false,
     0
   );
-
   const orbitPoints = orbitCurve.getPoints(100);
-
-  // Convertir les points en THREE.Vector3
-  const orbitPoints3D = orbitPoints.map(
-    (point) => new THREE.Vector3(point.x, 0, point.y)
-  );
-
-  // Appliquer l'inclinaison aux points de la trajectoire
-  const inclinationMatrix = new THREE.Matrix4().makeRotationX(inclination);
-  orbitPoints3D.forEach((point) => {
-    point.applyMatrix4(inclinationMatrix);
-  });
-
-  const orbitGeometry = new THREE.BufferGeometry().setFromPoints(orbitPoints3D);
+  const orbitGeometry = new THREE.BufferGeometry().setFromPoints(orbitPoints);
   const orbitMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
   const orbit = new THREE.LineLoop(orbitGeometry, orbitMaterial);
-
+  orbit.rotation.x = Math.PI / 2;
   scene.add(orbit);
   orbitLines.push(orbit);
 });
@@ -101,7 +122,7 @@ camera.position.set(0, 0, 40);
 scene.add(camera);
 
 // Renderer
-const canvas = document.querySelector(".webgl");
+const canvas = document.querySelector("canvas.webgl");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 
@@ -109,12 +130,38 @@ renderer.setSize(sizes.width, sizes.height);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+// Modal
+const modal = document.getElementById("modal");
+const planetName = document.getElementById("planet-name");
+const planetInfo = document.getElementById("planet-info");
+
+// Event listener for planet click
+planetMeshes.forEach((planetData) => {
+  const { mesh, info } = planetData;
+  console.log(mesh);
+  mesh.addEventListener("mouseenter", () => {
+    console.log(planetData);
+    // Show modal
+    planetName.textContent = planetData.name;
+    planetInfo.textContent = info;
+    modal.style.display = "block";
+  });
+});
+
+// Event listener for modal close button
+const closeButton = document.querySelector(".close");
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
 // Animation
+
 const animate = () => {
   controls.update();
 
   planetMeshes.forEach((planetData) => {
     const { mesh, distance, speed } = planetData;
+
     const angle = Date.now() * speed;
 
     const x = Math.cos(angle) * distance;
@@ -125,20 +172,7 @@ const animate = () => {
 
   renderer.render(scene, camera);
 
-  requestAnimationFrame(animate);
+  window.requestAnimationFrame(animate);
 };
 
 animate();
-
-window.addEventListener("resize", () => {
-  // Mettre à jour les tailles
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Mettre à jour la caméra
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Mettre à jour le rendu
-  renderer.setSize(sizes.width, sizes.height);
-});
